e8d7f7cf8e87504029f0d534b7231bfcfc1486e8
Brroodjee <bo_harmsen@outlook.com>
2024-05-31 18:54:12 +0200

    veel styling gedaan

--- src/main/webapp/js/productenThuis.js
+++ src/main/webapp/js/productenThuis.js
@@ -1,4 +1,5 @@
 function populateDropdown() {
+    console.log("populateDropdown() function called");
     fetch('http://localhost:4711/eet-share/login', {
         method: 'GET',
         headers: {
@@ -21,20 +22,13 @@ function populateDropdown() {
         .catch(error => console.error('Error:', error));
 }
 
-document.getElementById("ProductInHuis__button--plaatsToevoegen").addEventListener('click', function() {
-    const container = document.querySelector(".grid-container");
-    const template = document.getElementById("productInHuis__template");
-    const clone = template.content.cloneNode(true);
-    container.appendChild(clone);
-
-    populateDropdown();
-});
-
 document.getElementById("addRowButton").addEventListener('click', function() {
     const table = document.querySelector("#theTable");
     const tableTemplate = document.getElementById("productInHuis__Table-template");
     const cloneTable = tableTemplate.content.cloneNode(true);
     table.appendChild(cloneTable);
+
+    populateDropdown()
 });
 //https://stackoverflow.com/questions/44586479/adding-new-div-on-button-click
 //https://www.sitepoint.com/community/t/adding-rows-to-table-with-button-to-remove/434396/5
